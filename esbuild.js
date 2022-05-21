d6c706aad7b68730e6086b5b7ce97649675fa0b1
Nirintsoa <hhery889@gmail.com>
2022-05-21 14:09:03 +0300

    feat(bundler): build with version

--- esbuild.js
+++ esbuild.js
@@ -1,13 +1,22 @@
-import { build } from "esbuild"
+import { buildSync } from "esbuild"
+import fs from 'fs'
 
+const packages = JSON.parse(fs.readFileSync('package.json').toString())
 const isProd = process.env.NODE_ENV === 'prod'
 
-build({
-    entryPoints: ['src/index.js'],
-    bundle: true,
-    minify: isProd,
-    outfile: 'dist/uploader.min.js'
-})
-.then(() => {
-    console.log('Build')
-})
+if (isProd) {
+    buildSync({
+        entryPoints: ['src/index.js'],
+        bundle: true,
+        minify: true,
+        outfile: `dist/uploader-${packages.version}.js`
+    })
+}
+
+if (!isProd) {
+    buildSync({
+        entryPoints: ['src/index.js'],
+        bundle: true,
+        outfile: 'dist-dev/uploader.js'
+    })
+}
