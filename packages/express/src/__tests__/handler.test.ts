e03fec1a159a0ccf0082ddb8d694fe7fd44d672b
Hery Nirintsoa <hery.rakotoariniaina@ethumada.com>
2024-06-24 09:13:58 +0300

    feat: add some test for express

--- packages/express/src/__tests__/handler.test.ts
+++ packages/express/src/__tests__/handler.test.ts
@@ -19,7 +19,7 @@ describe(handler.name, () => {
     expect(res.status).toBe(400);
   });
 
-  it("/upload should throw invalid parameter", async () => {
+  it("/uploadStatus should return last chunk", async () => {
     const app = express();
     const fakeService = {
       uploadStatus: async (query: { chunkCount: number; fileId: string }) => {
@@ -27,7 +27,48 @@ describe(handler.name, () => {
       },
     } as RCUServiceInterface;
     app.use(handler(fakeService));
+    const res = await supertest(app)
+      .get("/uploadStatus")
+      .query({ fileId: "file.txt", chunkCount: "1" })
+      .send();
+    expect(res.body.lastChunk).toBe(0);
+  });
+
+  it("/upload should throw invalid parameter", async () => {
+    const app = express();
+    const fakeService = {
+      async uploadStatus() {
+        return { lastChunk: 0 };
+      },
+      async upload() {
+        return { message: "success" };
+      },
+    } as RCUServiceInterface;
+    app.use(handler(fakeService));
     const res = await supertest(app).post("/upload").send();
     expect(res.status).toBe(400);
+  });
+
+  it("/upload should return success message", async () => {
+    const app = express();
+    const fakeService = {
+      async uploadStatus() {
+        return { lastChunk: 0 };
+      },
+      async upload() {
+        return { message: "success" };
+      },
+    } as RCUServiceInterface;
+    app.use(handler(fakeService));
+    const res = await supertest(app)
+      .post("/upload")
+      .field("fileId", "package.json")
+      .field("chunkNumber", "1")
+      .field("originalFilename", "package.json")
+      .field("chunkCount", "1")
+      .field("chunkSize", "1")
+      .field("fileSize", "1")
+      .attach("file", Buffer.from("{}"), "package.json");
+    expect(res.body.message).toBe("success");
   });
 });
